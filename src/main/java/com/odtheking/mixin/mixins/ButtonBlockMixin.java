3c14a25f022f637c3ba9d348c1282400e0bd8e19
Hugo <hugo@zuperito.xyz>
2025-11-22 11:31:19 +0100

    fix: secretHitboxes

--- src/main/java/com/odtheking/mixin/mixins/ButtonBlockMixin.java
+++ src/main/java/com/odtheking/mixin/mixins/ButtonBlockMixin.java
@@ -1,40 +1,43 @@
-//package com.odtheking.mixin.mixins;
-//
-//import com.odtheking.odin.features.impl.dungeon.SecretHitboxes;
-//import net.minecraft.block.*;
-//import net.minecraft.block.enums.BlockFace;
-//import net.minecraft.state.property.Properties;
-//import net.minecraft.util.function.BooleanBiFunction;
-//import net.minecraft.util.math.BlockPos;
-//import net.minecraft.util.math.Direction;
-//import net.minecraft.util.shape.VoxelShape;
-//import net.minecraft.util.shape.VoxelShapes;
-//import net.minecraft.world.BlockView;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-//
-//import java.util.Map;
-//import java.util.function.Function;
-//
-//@Mixin(ButtonBlock.class)
-//public abstract class ButtonBlockMixin extends WallMountedBlock {
-//
-//    public ButtonBlockMixin(Settings settings) {
-//        super(settings);
-//    }
-//
-//    private Function<BlockState, VoxelShape> fullShapeFunction() {
-//        Map<BlockFace, Map<Direction, VoxelShape>> map = VoxelShapes.createBlockFaceHorizontalFacingShapeMap(Block.createCuboidZShape(16.0, 16.0, 12.0, 16.0));
-//        return this.createShapeFunction(
-//                state -> (VoxelShape)((Map)map.get(state.get(FACE))).get(state.get(FACING))
-//        );
-//    }
-//
-//    @Inject(method = "getOutlineShape", at = @At("RETURN"), cancellable = true)
-//    private void getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
-//        if (SecretHitboxes.INSTANCE.getEnabled() && SecretHitboxes.INSTANCE.getButton())
-//            cir.setReturnValue(this.fullShapeFunction().apply(state));
-//    }
-//}
+package com.odtheking.mixin.mixins;
+
+import com.odtheking.odin.features.impl.dungeon.SecretHitboxes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.function.Function;
+
+@Mixin(ButtonBlock.class)
+public abstract class ButtonBlockMixin extends FaceAttachedHorizontalDirectionalBlock {
+    protected ButtonBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Unique
+    private Function<BlockState, VoxelShape> fullShapeFunction() {
+        Map<AttachFace, Map<Direction, VoxelShape>> map = Shapes.rotateAttachFace(Block.boxZ(16.0, 16.0, 12.0, 16.0));
+        return this.getShapeForEachState(
+                blockState -> (map.get(blockState.getValue(FACE))).get(blockState.getValue(FACING))
+        );
+    }
+
+    @Inject(method = "getShape", at = @At("RETURN"), cancellable = true)
+    void getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext, CallbackInfoReturnable<VoxelShape> cir) {
+        if (SecretHitboxes.INSTANCE.getEnabled() && SecretHitboxes.INSTANCE.getButton())
+            cir.setReturnValue(this.fullShapeFunction().apply(blockState));
+    }
+}
--- src/main/java/com/odtheking/mixin/mixins/LeverBlockMixin.java
+++ src/main/java/com/odtheking/mixin/mixins/LeverBlockMixin.java
@@ -1,24 +1,24 @@
-//package com.odtheking.mixin.mixins;
-//
-//import com.odtheking.odin.features.impl.dungeon.SecretHitboxes;
-//import net.minecraft.block.*;
-//import net.minecraft.util.math.BlockPos;
-//import net.minecraft.util.shape.VoxelShape;
-//import net.minecraft.util.shape.VoxelShapes;
-//import net.minecraft.world.BlockView;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.*;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-//
-//@Mixin(LeverBlock.class)
-//public abstract class LeverBlockMixin extends WallMountedBlock {
-//    protected LeverBlockMixin(Settings settings) {
-//        super(settings);
-//    }
-//
-//    @Inject(method = "getOutlineShape", at = @At("RETURN"), cancellable = true)
-//    private void getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
-//        if (SecretHitboxes.INSTANCE.getEnabled() && SecretHitboxes.INSTANCE.getLever())
-//            cir.setReturnValue(VoxelShapes.fullCube());
-//    }
-//}
+package com.odtheking.mixin.mixins;
+
+import com.odtheking.odin.features.impl.dungeon.SecretHitboxes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.LeverBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LeverBlock.class)
+public class LeverBlockMixin {
+    @Inject(method = "getShape", at = @At("RETURN"), cancellable = true)
+    void getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext, CallbackInfoReturnable<VoxelShape> cir) {
+        if (SecretHitboxes.INSTANCE.getEnabled() && SecretHitboxes.INSTANCE.getLever()) {
+            if (blockPos.getX() >= 58 && blockPos.getX() <= 62 && blockPos.getY() >= 133 && blockPos.getY() <= 136 && blockPos.getZ() == 142) return;
+            cir.setReturnValue(Shapes.block());
+        }
+    }
+}
